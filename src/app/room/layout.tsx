d2ee92b0c898653013fb1f6115e367ed6bae47c3
i.raresandrei <i.raresandrei@yahoo.com>
2025-12-08 08:08:54 +0200

    2nd 1

---
+++ src/app/room/layout.tsx
@@ -0,0 +1,3 @@
+import "@liveblocks/react-ui/styles.css";
+import "@liveblocks/react-tiptap/styles.css";
+import "./styles/globals.css"
---
+++ src/app/room/page.tsx
@@ -0,0 +1,10 @@
+import { Room } from "./Room";
+import { Editor } from "./Editor";
+
+export default function Page() {
+  return (
+    <Room>
+      <Editor />
+    </Room>
+  );
+}
